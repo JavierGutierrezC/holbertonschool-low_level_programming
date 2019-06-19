014791e0b63cb5dbd360b55844fd3b1d19a25e1a
JavierGutierrezC <955@holbertonschool.com>
2019-06-19 23:46:01 +0000

    fixed return

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,12 @@ int main(void)
 	int integro;
 	long int longint;
 	long long int llongint;
-	float decimal:
-		printf("size of a char: %ld byte(s)\n", sizeof(caracter));
-		printf("size of a int: %ld byte(s)\n", sizeof(integro));
-		printf("size of a long int: %ld byte(s)\n", sizeof(longint));
-		printf("size of a long long int: %ld bytes(s)\n", sizeof(llongint));
-		printf("size of a float: %ld bytes(s)\n", sizeof(decimal));
+	float decimal;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(caracter));
+	printf("size of a int: %ld byte(s)\n", sizeof(integro));
+	printf("size of an long int: %ld byte(s)\n", sizeof(longint));
+	printf("size of a long long int: %ld bytes(s)\n", sizeof(llongint));
+	printf("size of a float: %ld bytes(s)\n", sizeof(decimal));
 	return (0);
 }
