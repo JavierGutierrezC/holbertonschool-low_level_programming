93fd7de6e0b7f3a412c23fb024fc4389fcd3703b
JavierGutierrezC <955@holbertonschool.com>
2019-06-19 23:52:40 +0000

    fixed caps

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int llongint;
 	float decimal;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(caracter));
-	printf("size of an int: %ld byte(s)\n", sizeof(integro));
-	printf("size of a long int: %ld byte(s)\n", sizeof(longint));
-	printf("size of a long long int: %ld bytes(s)\n", sizeof(llongint));
-	printf("size of a float: %ld bytes(s)\n", sizeof(decimal));
+	printf("Size of a char: %ld byte(s)\n", sizeof(caracter));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integro));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longint));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(llongint));
+	printf("Size of a float: %ld bytes(s)\n", sizeof(decimal));
 	return (0);
 }
