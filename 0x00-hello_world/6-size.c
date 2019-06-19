4e4d6fbbedcbeb1e0b97805099ace2c007dc547d
JavierGutierrezC <955@holbertonschool.com>
2019-06-19 23:32:11 +0000

    fixed comas

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char caracter;
+	int integro;
+	long int longint;
+	long long int llonont;
+	float decimal:
+		printf("size of a char: %ld byte(s)\n", sizeof(caracter));
+		printf("size of a int: %ld byte(s)\n", sizeof(integro));
+		printf("size of a long int: %ld byte(s)\n", sizeof(longint));
+		printf("size of a long long int: %ld bytes(s)\n", sizeof(llongint));
+		printf("size of a float: %ld bytes(s)\n", sizeof(decimal));
+	return (0);
+}
