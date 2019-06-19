afeb02bb7d96d78978fc0284c9bc7ca8072102b6
JavierGutierrezC <955@holbertonschool.com>
2019-06-19 23:55:47 +0000

    fixed Bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(caracter));
 	printf("Size of an int: %ld byte(s)\n", sizeof(integro));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longint));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(llongint));
-	printf("Size of a float: %ld bytes(s)\n", sizeof(decimal));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(llongint));
+	printf("Size of a float: %ld byte(s)\n", sizeof(decimal));
 	return (0);
 }
