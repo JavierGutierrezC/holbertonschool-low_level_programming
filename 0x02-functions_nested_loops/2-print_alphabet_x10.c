ae6e5ae678af62f8392ae225b683e9d31d2646c5
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-25 22:29:51 +0000

    take one line out

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- * print_alphabet - prints alphabet 10 times
+ *
+ * print_alphabet_x10 - prints alphabet 10 times
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int alph;
 	int r;
 
-	for (r = 0; r <= 10; r++)
+	for (r = 0; r <= 9; r++)
 	{
 
 		for (alph = 'a'; alph <= 'z'; alph++)
