ff1936408e9f8870d921cd48895bb25b45f5b9f7
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-25 23:25:10 +0000

    change upper to lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,18 @@
  * _islower - checks for lower case letters.
  * @c: checks to see if char is lower case
  *
- * Return: Always 0
+ * Return: Always 1 if true, 0 if false.
  */
 
 int _islower(int c)
 {
 
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
 	{
 		return (0);
 	}
-	else
-		return (1);
 }
