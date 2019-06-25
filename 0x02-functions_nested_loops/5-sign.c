6de39f0d24b2b63fb8220b71f82ea08030205579
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-25 23:48:24 +0000

    fix commands

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,32 +1,27 @@
 #include "holberton.h"
 /**
- * print_sign writes +, - or 0 depending on the number given
- * @n: character to print
+ * print_sign - writes + - or 0 to know what number's sign
+ * @n: The character to print
  *
- * Return 1 if true or -1 if it is false and o if it is zero
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-
-	if (n < 0)
-
-	{
-		_putchar('-');
-		return (-1);
-	}
-
 	if (n == 0)
-
 	{
 		_putchar('0');
 		return (0);
 	}
-
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 	return (0);
 }
