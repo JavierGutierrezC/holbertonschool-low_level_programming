be6afef85e6d28eafab092cd27cb2733f89be125
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-25 23:13:52 +0000

    add space

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,26 @@
 #include "holberton.h"
 
 /**
+ * print_alphabet_x10 - writes the alphabet between a to z in 5 lines.
  *
- * print_alphabet_x10 - prints alphabet 10 times
  *
  * Return: On success 1.
- * On error, -1 is returned, and eroro is set-
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet_x10(void)
 {
-	int alph;
-	int r;
+	int a2 = 0;
 
-	for (r = 0; r <= 9; r++)
+	while (a2 < 10)
 	{
+		char a = 'a';
 
-		for (alph = 'a'; alph <= 'z'; alph++)
-
+		while (a <= 'z')
 		{
-		_putchar(alph);
+			_putchar(a);
+			a = a + 1;
 		}
-	_putchar('\n');
+		_putchar('\n');
+		a2 = a2 + 1;
 	}
 }
