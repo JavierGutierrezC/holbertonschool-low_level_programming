2a5fa1c65b607315c4d33dc45d6da100d12af13c
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-25 17:33:45 +0000

    fix main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
