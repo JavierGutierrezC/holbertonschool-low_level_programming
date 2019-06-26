2cb36d03addde4c6b514b4561a518aed9f7d5a5e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-26 00:11:34 +0000

    fix comments

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,20 +1,24 @@
 #include "holberton.h"
-
 /**
+ * _abs - return absolut value.
+ * @r: Number to transfor into a positive number
  *
- *
- *
- *
- *
+ * Return: is always a positive number.
+ * Negative numbers are turned into positive
  */
-
 int _abs(int r)
 {
-	if( r > 0)
+	if (r > 0)
 	{
-		return(1);
-			}
+		return (r);
+	}
+	if (r == 0)
+	{
+		return (0);
+	}
 	if (r < 0)
 	{
-		return(1);
+		return (r * -1);
 	}
+	return (0);
+}
