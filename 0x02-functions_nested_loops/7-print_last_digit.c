8d31de6b9fbac7604aa275f04d4e41b2dd7eebc0
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-26 01:19:00 +0000

    fixed other things

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,18 +4,23 @@
  * @n: integer to use
  *
  * Print last number
+ *
+ * Return is the same as the integer
  */
 
 
 int print_last_digit(int n)
+
 {
 	n = n % 10;
 
 	if (n < 0)
+
 	{
 		n = n * -1;
 	}
 
 	_putchar('0' + n);
+
 	return (n);
 }
