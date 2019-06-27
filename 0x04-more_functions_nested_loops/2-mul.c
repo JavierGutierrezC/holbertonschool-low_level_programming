19af173991a7bcb6d5143fa6f1b3c29712c2f1eb
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-27 19:45:34 +0000

    fixed return

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - funtion to multiply digits
+ * @a digit to multiply
+ * @b digit to multiply
+ * Return: - is the multiplication of the 2 digits
+ */
+
+int mul(int a, int b)
+{
+
+	int mult = a * b;
+
+	return (mult);
+
+}
