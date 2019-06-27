d8a7029f90ae89c3acc2ec058586c840d99c23fd
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-27 18:24:58 +0000

    changed c is greater than

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - used to select characters that are leters in uppercase.
  *
- * Return: if it is an uppercase letter reurn is 1 if not it is 0.
+ * Return: - if it is an uppercase letter reurn is 1 if not it is 0.
  */
 
 int _isupper(int c)
 {
-	if (c <= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
