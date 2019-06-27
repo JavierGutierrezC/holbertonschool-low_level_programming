be4b1df6cea2f947a4de19ac56588d70ca6f237b
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-27 19:51:38 +0000

    erased spaces

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,14 +4,12 @@
  * mul - funtion to multiply digits
  * @a digit to multiply
  * @b digit to multiply
- * Return: - is the multiplication of the 2 digits
+ * Return: - is the multiplication of the 2 digits a and b
  */
 
 int mul(int a, int b)
 {
-
 	int mult = a * b;
 
 	return (mult);
-
 }
