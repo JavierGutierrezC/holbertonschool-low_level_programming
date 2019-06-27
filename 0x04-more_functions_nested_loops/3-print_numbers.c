956b79b55311af0e5944adfc144425fcf16a28f1
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-27 20:45:06 +0000

    added comments

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_numbers - prints a series of numbers
+ * @num: - variable of the function
+ * Return: - _putchar the numbers from 0 to 9
  */
 void print_numbers(void)
 {
