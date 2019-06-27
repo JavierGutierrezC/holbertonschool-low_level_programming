693aa07826dd0c2a869174a4615fc0ceb460fb1e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-27 21:00:14 +0000

    added comments

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - prints a series of numbers
  * @an: - variable of the function
- * Return: - _putchar the numbers from 0 to 9
+ * Return: - shows the numbers from 0 to 9
  */
 
 void print_numbers(void)
