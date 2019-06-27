0c83f2a5e851403afbb5358de10ef69b89f224f9
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-27 18:20:20 +0000

    add spaces in comments

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+
 /**
  * _isupper - used to select characters that are leters in uppercase.
  *
  * Return: if it is an uppercase letter reurn is 1 if not it is 0.
  */
+
 int _isupper(int c)
 {
 	if (c <= 65 && c <= 90)
