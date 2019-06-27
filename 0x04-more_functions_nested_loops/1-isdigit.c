a3f8d3de8a08b4e2f304bd4f564fbcf5841e33e9
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-27 18:34:21 +0000

    erase final dot of comments

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
+
 /**
- * _isdigit - will check for characters that are digits.
+ * _isdigit - will check for characters that are digits
  *
- * Return: 1 if it is a digit 0 for anything else.
+ * Return: 1 if it is a digit 0 for anything else
  */
+
 int _isdigit(int c)
 {
-	if (c >= 48 && c >= 57)
+	if (c <= 48 && c >= 57)
 	{
 		return (0);
 	}
