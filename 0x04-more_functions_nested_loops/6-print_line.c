e646bc1083a36f8cf79f97dbab102783f1b223c5
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-28 02:06:16 +0000

    add comments

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_line - print a character in a cascade shape
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
