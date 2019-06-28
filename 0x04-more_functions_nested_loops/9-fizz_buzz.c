88686cab512c2bb76334f7e91fb45b53bcc36b63
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-06-28 22:33:53 +0000

    add closing brakets

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Function wich will print the numbers asked in the statement
+ *
+ *
+ * Reurn: 0 if true
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 != 0))
+			printf("Fizz ");
+		if ((a % 3 != 0) && (a % 5 == 0) && (a != 100))
+			printf("Buzz ");
+		if ((a % 3 == 0) && (a % 5 == 0))
+			printf("FizzBuzz ");
+		if ((a % 3 != 0) && (a % 5 != 0))
+			printf("%d ", a);
+		if ((a % 3 != 0) && (a % 5 == 0) && (a == 100))
+			printf("Buzz");
+	}
+	printf("\n");
+	return (0);
+}
