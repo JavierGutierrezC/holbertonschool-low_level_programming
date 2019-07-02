520cfbd3b8564443820464ae4184ddf536a723f0
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-02 22:42:39 +0000

    add if

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,12 @@ void puts2(char *str)
 {
 	int n = 0;
 
-	for (n = 0; str[n] != '\0'; n = n + 2)
+	for (n = 0; str[n] != '\0'; n++)
 	{
-		_putchar(str[n]);
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
 	}
 	_putchar ('\n');
 }
