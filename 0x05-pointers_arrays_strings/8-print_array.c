4dfee0c9153b7270216fcfb2c3aadcfc0b8729ed
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-03 02:41:22 +0000

    add new line

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,16 @@ void print_array(int *a, int n)
 	int mas;
 
 	for (mas = 0; mas < n; mas++)
+	{
 		if (mas < n - 1)
 		{
 			printf("%d, ", a[mas]);
-		}
+	}
 	else
 	{
-		printf("%d\n", a[mas]);
+		printf("%d", a[mas]);
 	}
 
+	}
+	printf("\n");
 }
