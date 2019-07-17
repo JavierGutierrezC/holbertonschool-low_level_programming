a93aa7149733612ee783f74abfaba98f5c5c399e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-17 04:13:40 +0000

    initialize variables

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,39 +10,39 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *S;
-	int x;
-	int y;
-	int z;
-	int w;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	int w = 0;
 
 	if (s1 != NULL)
 	{
-		for (x = 0; s1[a] != '\0'; x++)
+		for (x = 0; s1[x] != '\0';x++)
 		{
 		}
 	}
 	if (s2 != NULL)
 	{
-		for (y = 0; s2[b] != '\0'; y++)
+		for (y = 0; s2[y] != '\0';y++)
 		{
 		}
-		S = malloc(x + y +1);
-		if (s == NULL)
+	}
+		S = malloc(x + y + 1);
+		if (S == NULL)
 		{
 			return (NULL);
 		}
-		for (z = 0; z < a + b; z++)
+		for (z = 0; z < x + y;z++)
 		{
-			if (c < a)
+			if (z < x)
 			{
-				S[c] = s1[z];
+				S[z] = s1[z];
 			}
 			else
 			{
-				S[c] = s2[s2[z];
+				S[z] = s2[w];
 					  w++;
 			}
 		}
-			return (S);
+		return (S);
 	 }
-}
