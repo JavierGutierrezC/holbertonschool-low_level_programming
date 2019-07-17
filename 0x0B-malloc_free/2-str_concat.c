2e04a3edeaedb5f70cb139909af50491907a6c38
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-17 03:28:08 +0000

    He who is not courageous ...

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *S;
+	int x;
+	int y;
+	int z;
+	int w;
+
+	if (s1 != NULL)
+	{
+		for (x = 0; s1[a] != '\0'; x++)
+		{
+		}
+	}
+	if (s2 != NULL)
+	{
+		for (y = 0; s2[b] != '\0'; y++)
+		{
+		}
+		S = malloc(x + y +1);
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+		for (z = 0; z < a + b; z++)
+		{
+			if (c < a)
+			{
+				S[c] = s1[z];
+			}
+			else
+			{
+				S[c] = s2[s2[z];
+			}
+		}
+			return (S);
+	 }
+}
