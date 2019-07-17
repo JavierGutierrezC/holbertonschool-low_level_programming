cce1b9f1a9893d18e30662f5ad2a736901b92fd0
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-17 05:08:16 +0000

    fixed spaces

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * free_grid - function
+ * @grid: int
+ * @height: int
+ * Return:
  *
  */
 void free_grid(int **grid, int height)
 {
 	int x;
 
-	for(x = (height - 1); x >= 0; x--)
+	for (x = (height - 1); x >= 0; x--)
 	{
 		free(grid[x]);
 	}
-	free (grid);
+	free(grid);
 }
