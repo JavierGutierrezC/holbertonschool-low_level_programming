c980e12026db29e01a40f7cc44659c955da16b60
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-19 03:11:04 +0000

    add min to x

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,30 +1,34 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * array_range - function that creates an array of integers
  * @min: int value
  * @max: int value
  * Return: Pointer of array
  */
+
 int *array_range(int min, int max)
 {
 	int *x;
 	int y;
 	int z;
 
-	z = max - min;
-
 	if (min > max)
 	{
 		return (NULL);
 	}
+
+	z = max - min;
+
 	x = malloc(sizeof(x) * (z + 1));
+
 	if (x == NULL)
 	{
 		return (NULL);
 	}
-	for (y = 0; y <= z; y++)
+	for (y = 0; min <= z; y++)
 	{
 		x[y] = min;
 		min++;
