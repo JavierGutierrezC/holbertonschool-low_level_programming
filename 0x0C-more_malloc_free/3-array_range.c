a7cf8db21b4217a7ee8284325aa4480afa688910
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-19 02:57:52 +0000

    add variable z

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,12 +11,15 @@ int *array_range(int min, int max)
 {
 	int *x;
 	int y;
+	int z;
+
+	z = (max - min);
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	x = malloc(sizeof(x) * (max - min + 1));
+	x = malloc(sizeof(x) * (z + 1));
 	if (x == NULL)
 	{
 		return (NULL);
