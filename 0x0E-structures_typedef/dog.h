56c7490d7d1856066c3424e901c17d6e0256446e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-22 23:30:16 +0000

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _dog.h_
+#define _dog.h_
+/**
+ * struct dog - Dog information
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
+int _putchar (char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
