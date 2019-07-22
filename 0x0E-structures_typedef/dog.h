6fa76be1aadb426ba7462aa495dcfe36b54618bf
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-22 23:34:33 +0000

    change headers

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG.H
-#define DOG.H
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Dog information
  * @name: name of dog
@@ -13,6 +13,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
 
 int _putchar (char c);
@@ -20,4 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
