1412d04b9efeef843b3174e0823d5be1bfa2b054
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-23 04:16:03 +0000

    fixed all keys returns are not inside if

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - Prints dogs information from structure
+ * @d: Structure with dog information
+ *
+ * Return: ;
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+		{
+			d->name = "nil";
+			printf("Name: %s\n", d->name);
+		}
+	printf("Age: %f\n", d->age);
+
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+		{
+			d->owner = "nil";
+			printf("Owner: %s\n", d->owner);
+		}
+}
