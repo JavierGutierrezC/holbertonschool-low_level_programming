616619c4073ad9f24ced6548a01eca215a8d5430
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-24 20:17:25 +0000

    Add comments

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
 #include "stdlib.h"
 /**
- *
- *
- *
- *
+ * print_name - prints a name in lower and upper case
+ * @name: pointer directed to the name in the func
+ * @f: point to the name in the function
  *
  *
  */
