b84945782989586c2f6da2c4d359fd1054a12115
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-24 23:41:01 +0000

    add spaces, add condition

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for the integer
+ * @array: array to print
+ * @size: Size of the array
+ * @cmp: pointer that point to the array
+ * Return: -1 for 0 or null
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (x = 0; x < size; x++)
+		{
+			if (cmp(array[x]) == 1)
+			{
+				return (x);
+			}
+		}
+	}
+	return (0);
+}
