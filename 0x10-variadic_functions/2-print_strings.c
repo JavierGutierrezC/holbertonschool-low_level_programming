c3350d32a0ed31103bacccef31d5f52983cf175e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-28 02:29:55 +0000

    Add NULL to condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", y);
 		}
-		if (x < (n - 1) && separator)
+		if (x < (n - 1) && separator != NULL)
 		    printf("%s", separator);
 	}
 		printf("\n");
