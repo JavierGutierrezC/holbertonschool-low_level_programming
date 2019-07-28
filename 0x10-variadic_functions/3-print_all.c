cfe2a9dfa7a47db7bc16d2e4eaaeb68fdb5d1dc6
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-28 03:40:19 +0000

    fixed char *

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -27,7 +27,7 @@ void print_all(const char * const format, ...)
 			printf("%f", va_arg(valist, double));
 			break;
 		case 's':
-			s = va_arg(valist, char*);
+			s = va_arg(valist, char *);
 			if (s == NULL)
 			{
 				printf("(nil)");
