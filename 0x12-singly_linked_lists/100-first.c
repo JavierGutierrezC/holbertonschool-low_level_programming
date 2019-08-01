60b9481115a560cf6371d6d50912599d54dcea2b
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-01 23:22:00 +0000

    Tortue

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * la_tortoise - prints
+ *
+ * Return: None
+ *
+ */
+void la_tortoise(void) __attribute__((constructor));
+
+	void la_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
