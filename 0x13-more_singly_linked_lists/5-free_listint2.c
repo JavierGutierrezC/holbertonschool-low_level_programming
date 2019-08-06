ec3e19dcd5e5a25d18b4a5519cc40ede0ce0af6b
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-06 15:18:40 +0000

    []free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - drees a list
+ * @head: Head of the list
+ *
+ *
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tem;
+
+	while (*head != NULL)
+	{
+		tem = (*head)->next;
+		free(*head);
+		(*head) = tem;
+	}
+	head = NULL;
+	free(tem);
+}
