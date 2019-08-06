6d9589f859ff1858c28f3bbded133f72212c0a01
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-06 16:29:01 +0000

    [Fixed]added condition for null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tem;
 
-	while (*head != NULL)
-	{
+	if (head == NULL)
+		return;
+
+		while (*head != NULL)
+		{
 		tem = (*head)->next;
 		free(*head);
 		(*head) = tem;
-	}
+		}
 	head = NULL;
 	free(tem);
 }
