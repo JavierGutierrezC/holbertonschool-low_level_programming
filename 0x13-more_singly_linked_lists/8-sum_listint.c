3b3bafd790e6a03030c79c4322dcd86e92edce14
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-06 16:41:13 +0000

    [Fixed]fixed spaces and comment

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -11,11 +11,11 @@ int sum_listint(listint_t *head)
 	int add = 0;
 
 	if (head == NULL)
-		return(0);
+		return (0);
 	while (head != NULL)
 	{
 		add = add + head->n;
 		head = head->next;
 	}
-	return(add);
+	return (add);
 }
