7bbcae4c58afedc431d988d714ff297359bfca94
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-06 18:39:47 +0000

    [fixed] return

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * get_nodeint_at_index - returrns a node from the list
+ * @head: First element in the list
+ * @index: index of the node
+ * Return: NULL if node doesn't exist
  *
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
