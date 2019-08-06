8ea44ea1ea20c892421f7520d2198af1219c1ec1
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-06 15:12:11 +0000

    []print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - prints content of list
+ * @h: start of a link list
+ * Return:Number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	for (; h != NULL;)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
