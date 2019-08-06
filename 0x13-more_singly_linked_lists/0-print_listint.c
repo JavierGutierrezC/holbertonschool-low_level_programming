c4c48fbeacbbf7aa2fa002e5b62667f8972e9471
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-06 15:49:03 +0000

    [Fixed]Add library

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_listint - prints content of list
  * @h: start of a link list
@@ -8,10 +9,10 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	int count;
 
 
-	while (h != NULL)
+	for (count = 0; h != NULL;)
 	{
 		printf("%d\n", h->n);
 		count++;
