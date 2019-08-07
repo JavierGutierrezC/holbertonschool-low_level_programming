3f91375f3271bea7a20f67a2847bda7aaec1c6b1
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-07 01:29:15 +0000

    [Fixed]added return NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * insert_nodeint_at_index - insert new node
+ * @head: first element of the list
+ * @idx: place where new node should be added
+ * @n: new node
+ * Return: Adress od new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -12,20 +13,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	act = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		return()NULL;
+		return (NULL);
 	new->n = n;
 	if (idx == 0)
 	{
 		new->next = act;
-		return(*head);
+		return (*head);
 	}
 		while (idx > 1)
 		{
 			act = act->next;
 			idx--;
-			if(!act)
+			if (!act)
 			{
 				free(new);
+				return (NULL);
 			}
 		}
 		next->next == act->next;
