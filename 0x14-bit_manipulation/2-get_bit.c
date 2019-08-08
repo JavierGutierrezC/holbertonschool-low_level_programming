2739c3668c4862dac71d507b6cbf8bceb1bbe76e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-08 23:11:09 +0000

    [fixed]betty and erased varaible declaration

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
+ * get_bit - returns the value of a bit at a given index
+ * @n: bit
+ * @index: Index
+ * Return: Value of a bit or -1 if error
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-n = n >> index
 
-	if (index > 63)
-		return(-1);
-return((n >> index) &1);
+		if (index > 63)
+			return (-1);
+	return ((n >> index) & 1);
 
 }
