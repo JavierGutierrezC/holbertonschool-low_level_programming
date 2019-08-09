a83b2e829de0032b46ae72c43f9b86ea38eae870
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-09 00:30:44 +0000

    [fix]changed order: 1 before index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_bit - sets the value of a bit to 1
+ * @n: value to enter
+ * @index: place to set the value
+ *
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n |= (1 << index);
+		return (1);
+}
