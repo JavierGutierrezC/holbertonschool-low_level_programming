019d10b6f92bbabf25850f6080b025ab8eca6d04
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-14 01:30:16 +0000

    []first comit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#incude "holberton.h"
+#include <stdio.h>
+/**
+ * main _ print first 50 fibonachi numbers
+ * Return: Zero
+ *
+ */
+int main(void)
+{
+	unsigned long compt, nom1, nom2, add;
+
+	nom1 = 0;
+	nom2 = 1;
+	for (compt = 0, compt < 50; compt++)
+	{
+		add = nom1 + nom2;
+		nom1 = nom2;
+		nom2 = add;
+		if (compt != 49)
+		{
+			printf("%lu, ", add);
+		}
+		else
+		{
+			printf("%lu\n", add);
+		}
+	}
+	return (0);
+}
