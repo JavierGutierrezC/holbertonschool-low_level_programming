f35c85f8f0b6f31f837d67e8d6236e1dfdf3a666
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-15 15:17:28 +0000

    [Fixed]changed , for ; and a variable

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,10 +39,10 @@ int main(int argc, char *argv[])
 	ficho = open(argv[1], O_RDONLY);
 	if (ficho == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 	}
-	ecr = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
+	fichc = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
 	if (fichc == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
