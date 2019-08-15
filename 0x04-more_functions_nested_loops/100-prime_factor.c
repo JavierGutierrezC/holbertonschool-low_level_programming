007ae78c29bb8de8361eab9387b40276ee0a0e18
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-15 23:04:59 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints largest prime factor  of anumber
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	long x;
+	long prime = 612852475143;
+
+	for (x = 2; x < prime; x++)
+	{
+		if ((prime % x) == '0')
+			prime = prime / x;
+	}
+	printf("%li\n", prime);
+	return (0);
+}
