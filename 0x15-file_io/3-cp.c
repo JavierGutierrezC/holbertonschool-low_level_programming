483d879789e71145476f4dd1b1199a41914c441b
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-15 15:39:35 +0000

    [Fixed]included closing in same function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,24 +1,5 @@
 #include "holberton.h"
 /**
- * closeall - closes all functions
- * @ficho: original
- * @fichc: copy
- *
- */
-void closeall(int ficho, int fichc)
-{
-	if (close(fichc) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fichc);
-		exit(100);
-	}
-	if (close(ficho) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ficho);
-		exit(100);
-	}
-}
-/**
  * main - copy contento of a file to another
  * @argc: count
  * @argv: array
@@ -34,35 +15,36 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			exit(97);
-	}
+		exit(97); }
 	ficho = open(argv[1], O_RDONLY);
 	if (ficho == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-	}
+		exit(98); }
 	fichc = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
 	if (fichc == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		exit(99); }
 	while (count == 1024)
 	{
 		count = read(ficho, buffer, 1024);
 		if (count == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			exit(98); }
 		ecr = write(fichc, buffer, count);
 		if (ecr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
-				exit(99);
-		}
-	}
-		closeall(ficho, fichc);
-		return (0);
+			exit(99); }}
+	if (close(ficho) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %\n", ficho);
+		exit(100); }
+	if (close(fichc) == -1)
+	{
+		dfrintf(STDERR_FILENO, "Error: Can't close fd %d\n", fichc);
+		exit(100); }
+	return (0);
 }
