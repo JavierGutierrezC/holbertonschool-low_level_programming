84cb55153b99a7402375be63e43541f7ce0d72a6
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-03 16:52:43 +0000

    [fix]arrey of 11 to 10

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,18 +9,18 @@ char *leet(char *arr)
 {
 	int x;
 	int y;
-	char letr[11] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
-	char new[11] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	char letr[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char new[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 
 	for (x = 0; arr[x] != '\0'; x++)
 	{
-	for (y = 0; y < 10; y++)
-	{
-	if (arr[x] == letr[y])
-	{
-	arr[x] = new[y];
-	}
-	}
+		for (y = 0; y < 10; y++)
+		{
+			if (arr[x] == letr[y])
+			{
+				arr[x] = new[y];
+			}
+		}
 	}
 	return (arr);
 }
