3a97ff222f8c7f9bf529ac8ebfaa0d9e48e24832
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-06 17:25:49 +0000

    [fix]Betty

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,34 +1,43 @@
 #include "dog.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
- *
+ * new_dog - creates new dog info
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: name of owner
+ * Return: NULL if fails
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *mid;
+	dog_t *flofy;
 	int x;
 	int y;
 	int z;
 	int w;
 
-	mid = malloc(sizeof(dog_t));
+	flofy = malloc(sizeof(dog_t));
 
-	if(mid == NULL)
+	if (flofy == '\0')
 	{
 		return (NULL);
 	}
-	for (x = 0; name[x] != '\0'; n++)
+	for (x = 0; name[x] != '\0'; x++) /*contador name*/
 		;
-	mid->name = malloc(sizeof(char) * (n + 1));
-	if (mid->name == NULL)
+	flofy->name = malloc(sizeof(char) * (x + 1));
+	if (flofy->name == NULL)
 	{
-		free(mid);
+		free(flofy);
 		return (NULL);
 	}
-	mid->age = age;
-	for (y = 0; owner[y] != '\0';y++)
+	flofy->age = age;
+	for (y = 0; owner[y] != '\0'; y++) /*contador owner*/
+		;
+	flofy->owner = malloc(sizeof(char) * (y + 1));
+	if (flofy->owner == NULL)
+	{
+		free(name);
+		free(flofy); /*free del mas pequeño al mas grande*/
+		return (NULL);
+	}
+	for (z = 0; z <= flofy; z++)
