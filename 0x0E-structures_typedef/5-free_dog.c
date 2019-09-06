3c4d79eda7bc36aca5d05b6ac55b44d7f4b99453
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-06 17:48:24 +0000

    [fix]added library

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * free_dog - initialize a variable of type struct
  * @d: pointer of structure
  * @name: name of dog
  * @age: age of dog
  * @owner: name of owner
- * Return:
+ * Return: 0
  */
 void free_dog(struct dog *d, char *name, float age, char *owner)
 {
