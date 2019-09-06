8b3f0e85b79e279985943db123b8f4a42fcb5e56
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-06 17:58:45 +0000

    [Fix]eliminated free name

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,19 +2,16 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * free_dog - initialize a variable of type struct
+ * free_dog - frees dog
  * @d: pointer of structure
- * @name: name of dog
- * @age: age of dog
- * @owner: name of owner
  * Return: 0
  */
-void free_dog(struct dog *d, char *name, float age, char *owner)
+void free_dog(dog_t *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
 		free(d->name);
-		free(d->age);
 		free(d->owner);
+		free(d);
 	}
 }
