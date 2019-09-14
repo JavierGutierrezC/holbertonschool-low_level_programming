07c8ec3e4d7ae07d0b5f5117663eef988bae898b
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-14 23:21:18 +0000

    fixed variable

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - returns a node from the list
+ * get_dnodeint_at_index - returns a node from the list
  * @head: First element of the list
- * @index: INdex of the node
+ * @index: Index of the node
  * Return: NULL if node doest exist or the index
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int x;
 
-	for (X = 0; x < index; x++)
+	for (x = 0; x < index; x++)
 	{
 		if (head->next == NULL)
 			return (NULL);
