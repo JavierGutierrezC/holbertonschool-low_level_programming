05de0001627882a1097f42a283052e4dd7cf5e53
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-15 00:56:44 +0000

    idx=NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,9 +29,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		act = act->next;
 		idx--;
-		if (!act)
+		if (act == NULL)
 		{
-			free(new);
 			return (NULL);
 		}
 	}
