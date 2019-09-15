92ed81740db0d15be960deddeb65b8a66667baa8
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-15 00:49:03 +0000

    added prev and new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,14 +1,17 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - insert new node
+ * insert_dnodeint_at_index - insert new node
  * @h: first element of the list
  * @idx: place where new node should be added
  * @n: new node
- * Return: Adress od new node
+ * Return: Adress of new node or NULL if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *act, *new;
+
+	if (h == NULL)
+		return (NULL);
 
 	act = *h;
 	new = malloc(sizeof(dlistint_t));
@@ -18,6 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = act;
+		new->prev = NULL;
 		*h = new;
 		return (*h);
 	}
@@ -33,5 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new->next = act->next;
 	act->next = new;
+	new->prev = act;
+	new->next->prev = new;
 	return (new);
 }
