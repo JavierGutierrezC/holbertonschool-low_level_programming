dc9fcf0f41be1723c412751a472e841c4a5d7735
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-29 15:55:56 +0000

    [Fix] prototypes

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -22,4 +22,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+size_t partitionf(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
 #endif
